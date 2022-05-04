0b57db039efb7ff64301314e24b41256d2b59bd6
Dedicatu1979 <88102275+Dedicatu1979@users.noreply.github.com>
2022-05-04 16:49:15 +0800

    Add files via upload

---
+++ pixelartmaker.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+# @Time : 2022/5/3 下午 5:29
+# @Author : Dedicatus1979
+# @File : pixelartmaker.py
+# @Software : PyCharm
+
+from os import system
+from PixelArtMaker import main
+
+if __name__ == '__main__':
+    main
+    system("pause")
